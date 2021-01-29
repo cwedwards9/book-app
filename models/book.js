997c44cd6b228e8d2388ff7b75e973c6843be16b
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-29 10:13:16 -0500

    Set up the model schema for the book collection in our db.

---
+++ models/book.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+// Create Project schema
+const BookSchema = new mongoose.Schema ({
+    title: {
+        type: String,
+        trim: true,
+    },
+    authors: {
+        type: String,
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    image: {
+        type: String,
+        trim: true
+    },
+    link: {
+        type: String,
+        trim: true
+    }
+});
+
+module.exports = mongoose.model("Book", BookSchema);
