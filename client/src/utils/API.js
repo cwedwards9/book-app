ffeb4ba6d118382d62f01fe68d8a0fcb141776fb
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-29 14:11:30 -0500

    Add 'returnBook' method to make a GET request to return books from db and 'deleteBook' to send a DELETE request and delete a book by id.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API = {
+    // Get request to call the API on the backend
     searchBooks(query) {
         return axios.get("/search", {
             params: {
@@ -8,9 +9,17 @@ const API = {
             }
         });
     },
-
+    // Post request to save a book to the db
     saveBook(query) {
         return axios.post("/api/books", query)
+    },
+    // Get request to rturn all saved books in the db
+    returnBooks() {
+        return axios.get("/api/books")
+    },
+    // delete a book, by id, in the db
+    deleteBook(id) {
+        return axios.delete(`/api/books/${id}`)
     }
 }
 
