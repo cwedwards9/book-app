40fbf833e38b3f14210a11074301c53dceb97d3e
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-29 12:11:38 -0500

    Add form to search for a book, making a call to the backend which calls our Google Books api and returns results and saves in our state.

--- client/src/pages/BookSearchForm.js
+++ client/src/pages/BookSearchForm.js
@@ -1,17 +1,50 @@
 import React, { Component } from "react";
+import BookSearchResults from "./BookSearchResults";
+import { API } from "../utils/API";
 
 class BookSearchForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { search: "", books: [] };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name] : evt.target.value
+        });
+    }
+    handleSubmit(evt) {
+        evt.preventDefault();
+
+        API.searchBooks(this.state.search)
+            .then(books => {
+                this.setState({ books: books.data });
+            })
+    }
+
     render() {
         return (
             <div className="BookSearchForm">
                 <h1>Search For a Book</h1>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input
                         type="text"
                         name="search"
-                        value=""
+                        value={this.state.search}
+                        onChange={this.handleChange}
                     />
+                    <button>Search</button>
                 </form>
+
+                {this.state.books.map(book => (
+                    <BookSearchResults
+                        key={book.id}
+                        id={book.id}
+                        info={book.volumeInfo}
+                    />
+                ))}
+
             </div>
         )
     }
