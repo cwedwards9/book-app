36545da18b6d65da32225000232f87f2eb1dd8e7
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-29 13:21:43 -0500

    Add state, and handleSave method that is called when a user clicks the save button. The state is set to be the current book and the book data is sent to the db to be saved.

--- client/src/pages/BookSearchResults.js
+++ client/src/pages/BookSearchResults.js
@@ -1,16 +1,45 @@
 import React, { Component } from "react";
+import { API } from "../utils/API";
+import "./BookSearch.css";
 
 class BookSearchResults extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: "", authors: "", description: "", image: "", link: "" };
+        this.handleSave = this.handleSave.bind(this);
+    }
+    handleSave() {
+        const { title, authors, description, imageLinks, infoLink } = this.props.info;
+        this.setState({
+            title: title, 
+            authors: authors, 
+            description: description, 
+            image: imageLinks.thumbnail, 
+            link: infoLink
+        }, () => {
+            API.saveBook(this.state);
+        });
+    }
     render() {
         const { title, authors, description, imageLinks, infoLink } = this.props.info;
         return (
-            <div className="BookSearchResults">
-                <p>{title}</p>
-                <p>{authors}</p>
-                <p>{description}</p>
-                <img src={imageLinks.thumbnail} alt={title}/>
-                <a href={infoLink}>More Info</a>
-            </div>
+            <main className="BookSearchResults">
+                <section className="BookSearchResults-Title">
+                    <div>
+                        <p>{title}</p>
+                        <p>{authors}</p>
+                    </div>
+                    <div>
+                        <a href={infoLink}>More Info</a>
+                        <button onClick={this.handleSave}>Save</button>
+                    </div>
+                </section>
+
+                <section className="BookSearchResults-Description">
+                    <img src={imageLinks.thumbnail} alt={title}/>
+                    <p>{description}</p>
+                </section>
+            </main>
         )
     }
 }
