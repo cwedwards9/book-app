d6c86cb0bb5fbe199ec367964c706205edd91d4b
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-29 11:41:38 -0500

    Add get route for handling a call to the Google books API that searches by user input.

--- routes/book.js
+++ routes/book.js
@@ -1,10 +1,19 @@
 const router = require("express").Router();
+const axios = require("axios");
 const Book = require("../models/book");
 
-
+// API Key environment variable setup
 require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
+// search for books with the Google Books API
+router.get("/search", (req, res) => {
+    console.log(req.query.q);
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${req.query.q}&maxResults=20&key=${apiKey}`)
+        .then(books => {
+            res.json(books.data.items);
+        })
+});
 
 // get - return all saved books 
 router.get("/api/books", (req, res) => {
