4baf6f7584f5b9f3afbbd138828b68597a0c55c0
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-29 11:37:46 -0500

    Import book routes.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const bookRoutes = require("./routes/book");
 
 const URL = process.env.MONGODB_URI || "mongodb://localhost/booksDB";
 
@@ -24,7 +24,7 @@ app.use(express.json());
 //     app.use(express.static('build'));
 // }
 
-
+app.use(bookRoutes);
 
 const PORT = process.env.PORT || 3001;
 
