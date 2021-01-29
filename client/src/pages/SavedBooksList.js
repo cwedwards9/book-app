c052bb389f4f3b96b952f3276d7d28ea652556cd
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-29 14:14:14 -0500

    GET all books from db on page load, save to state. Render the books state, passing down data to SavedBooksItem component. Create method to delete a book from the db by id.

--- client/src/pages/SavedBooksList.js
+++ client/src/pages/SavedBooksList.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
+import SavedBooksItem from "./SavedBooksItem";
+import { API } from "../utils/API";
 
 class SavedBooks extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { books: [] };
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    componentDidMount() {
+        API.returnBooks()
+            .then(books => {
+                this.setState({ books: books.data });
+            });
+    }
+    handleDelete(id) {
+        API.deleteBook(id)
+            .then(() => {
+                this.setState({
+                    books: this.state.books.filter(c => c._id !== id)
+                });
+            });
+    }
     render() {
         return (
             <div className="SavedBooks">
-                These are the saved books!
+                {this.state.books.map(book => (
+                    <SavedBooksItem 
+                        key={book._id}
+                        id={book._id}
+                        title={book.title}
+                        authors={book.authors}
+                        description={book.description}
+                        image={book.image}
+                        link={book.link}
+                        deleteBook={this.handleDelete}
+                    />
+                ))}
             </div>
         )
     }
