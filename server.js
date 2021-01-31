0515acefc078742fd2b79873d36266744c07fe03
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-31 14:28:24 -0500

    Add route to handle any undefined routes.

--- server.js
+++ server.js
@@ -27,6 +27,11 @@ if(process.env.NODE_ENV === 'production') {
 
 app.use(bookRoutes);
 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
