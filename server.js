83a5795ac81ba9366918834a0c41adeff0863aa1
Chase Edwards <chase.w.edwards9@gmail.com>
2021-01-31 13:27:31 -0500

    Fix path for running on production.

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
 }
 
 
